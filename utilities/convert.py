b50f75d746c429a64178bd289454b0b1f62f93ac
DanJenkins-Developer <djenkinsjg0617@gmail.com>
2025-09-05 11:14:12 -0500

    Added checks for file paths / directories

--- utilities/convert.py
+++ utilities/convert.py
@@ -2,11 +2,13 @@
 #
 # Help utility to convert python scripts to one line.
 #  
-# There's probably already a tool for this, but reinventing the wheel can be fun. 
+# There's probably already a tool for this, but reinventing the wheel can be fun.
+# 
+# The initial version of this program assumes that the original script is in a format that, 
+# once converted into a single line, will execute correctly when passed to the -Command / -c parameter. 
 
 import argparse
-import os
-import pathlib
+from pathlib import Path
 
 # Parse command line args
 parser = argparse.ArgumentParser()
@@ -14,6 +16,19 @@ parser.add_argument("source_file", help="Path to Powershell script.", type=str)
 parser.add_argument("target_directory", help="Target directory for the converted file.", type=str)
 args = parser.parse_args()
 
-# Check arguments
-print(args.source_file)
-print(args.target_directory)
+# Check if arguments exist
+sfile = Path(args.source_file)
+tdir = Path(args.target_directory)
+
+if (not sfile.is_file()):
+    msg = "convert.py: " + str(sfile) + ": No such file" 
+    exit(msg)
+
+if (not tdir.is_dir()):
+    msg = "convert.py: " + str(tdir) + ": No such directory"
+    exit(msg)
+
+print(sfile)
+print(tdir)
+
+# Open source file
