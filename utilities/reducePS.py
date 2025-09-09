c0ce4d7ef9817fbc5a28026adccc645b9915e3bc
Daniel Jenkins <djenkinsjg0617@gmail.com>
2025-09-08 20:08:01 -0500

    file rename

--- utilities/reducePS.py
+++ utilities/reducePS.py
@@ -1,21 +1,31 @@
 # Dan Jenkins 09/04/25
 #
-# Help utility to convert python scripts to one line.
+# Helper utility to convert Powershell scripts to one line to be used in Elastic response actions.
 # There's probably already a tool for this, but reinventing the wheel can be fun.
 # 
-# The initial version of this program assumes that the original script is in a format that, 
-# once converted into a single line, will execute correctly when passed to the -Command / -c parameter. 
+# The initial version of this program removes white space characters and comments from scripts,
+# but otherwise assumes that the original script is in a format that, once converted 
+# into a single line, will execute correctly when passed to the -Command / -c parameter. 
+
+def lineConditionsMet(l : str):
+    met = True
+    if (l == "" or 
+        l.startswith("#")):
+
+        met = False
+    
+    return met
 
 import argparse
 from pathlib import Path
 
-# Parse command line args
+# Parse command line args. Source powershell script and a target directory for the reduced script are required.
 parser = argparse.ArgumentParser()
 parser.add_argument("source_file", help="Path to Powershell script.", type=str)
 parser.add_argument("target_directory", help="Target directory for the converted file.", type=str)
 args = parser.parse_args()
 
-# Check if arguments exist
+# Check if the user provided arguments exist.
 sfile = Path(args.source_file)
 tdir = Path(args.target_directory)
 
@@ -27,12 +37,12 @@ if (not tdir.is_dir()):
     msg = f"convert.py: {str(tdir)} : No such directory"
     exit(msg)
 
-# Open source file
+# Open source file.
 with sfile.open('r') as script:
     content = script.readlines()
 
-# Modify list
-content = [line.strip() for line in content if (line.strip() != "" and not line.strip().startswith("#"))]
+# Modify list.
+content = [line.strip() for line in content if lineConditionsMet(line.strip())]
 
 # create one line with space splitting them
 one_line = ' '.join(content)
